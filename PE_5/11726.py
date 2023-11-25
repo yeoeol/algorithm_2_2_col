6eafde90f3e3c4ba49239edafa4444e014a37a0e
Yeoeol <publicemailgnsl@gmail.com>
2023-11-25 12:49:59 +0900

    baekjoon_11726_python

--- PE_5/11726.py
+++ PE_5/11726.py
@@ -1,18 +1,9 @@
-def solve():
-    l = [[0 for _ in range(n+1)] for _ in range(k+1)]
-    for i in range(1, n+1): # 0층 초기화
-        l[0][i] = i
+n = int(input())
 
-    for floor in range(1, k+1):
-        for ho in range(1, n+1):
-            summ = 0
-            for i in range(1, ho+1):
-                summ += l[floor-1][i]
-            l[floor][ho] = summ
-    print(l[k][n])
+l = [0 for _ in range(n+1)]
+l[0] = 1
+l[1] = 1
+for i in range(2, n+1):
+    l[i] = l[i-1]+l[i-2]
 
-T = int(input())
-for _ in range(T):
-    k = int(input()) #층
-    n = int(input()) #호
-    solve()
+print(l[n]%10007)
